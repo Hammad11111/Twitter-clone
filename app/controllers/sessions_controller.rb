4c5169558f006974ab3af4276bddc84ad89d2dc0
Hammad Khalid <hammadkhalid371@gmail.com>
2024-09-08 00:54:26 -0700

    This is a complete twitter clone website

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,13 @@
+
+
+    class SessionsController < Devise::SessionsController
+      before_action :configure_sign_in_params, only: [:create]
+  
+      private
+  
+      def configure_sign_in_params
+        devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password, :remember_me])
+      end
+    end
+
+  
